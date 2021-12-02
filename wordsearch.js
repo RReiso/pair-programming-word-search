cc19e359cd17e6a6394abe5d0c5731bbdc8fc9f2
Ruta Reisoglu <rutareiso@protonmail.com>
2021-12-02 00:52:49 +0000

    Find word in matrix. Pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let resultingArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let row = [];
+    for (let j = 0; j < matrix.length; j++) {
+      row.push(matrix[j][i]);
     }
-}
+    resultingArray.push(row);
+  }
+  return resultingArray;
+};
 
-module.exports = wordSearch
+const searchLines = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+};
+
+const wordSearch = (letters, word) => {
+  if (searchLines(letters, word)) {
+    return true;
+  }
+  const newMatrix = transpose(letters);
+  if (searchLines(newMatrix, word)) {
+    return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
+
+
